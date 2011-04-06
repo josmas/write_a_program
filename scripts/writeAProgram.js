4573ab7a9f7038f0e7be4d66253054c2420910ab
Jose Dominguez <jos@tchpc.tcd.ie>
2011-04-06 16:01:54 +0100

    skeleton for a jQuery plugin and test file

---
+++ scripts/writeAProgram.js
@@ -0,0 +1,8 @@
+(function( $ ){
+    $.fn.writeAProgram = function(){
+      //Do all the stuff in the plugin here
+      //for now an alert
+      alert('From the write a program plugin');
+    };
+})( jQuery );
+    
