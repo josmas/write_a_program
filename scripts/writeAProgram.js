48ebfeee33e350d40597d1e721f87a0742fb2940
Kasper Vervaecke <kaspervervaecke@gmail.com>
2011-04-07 16:12:14 +0100

    Further shape optimising, made them clonable and draggable

--- scripts/writeAProgram.js
+++ scripts/writeAProgram.js
@@ -1,8 +1,14 @@
 (function( $ ){
     $.fn.writeAProgram = function(){
-      //Do all the stuff in the plugin here
-      //for now an alert
-      alert('From the write a program plugin');
+      /*
+			Clone the shape on click
+			Append it to programContainer and make it draggable
+			*/
+			var shapeArr = $("#shapeContainer > div");
+			for (var i = 0; i < shapeArr.length; i++) {
+				$(shapeArr[i]).bind('mouseup' ,function() {
+					$(this).clone().appendTo("#programContainer").draggable();
+				});
+			}
     };
 })( jQuery );
-    
